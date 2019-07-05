5ef0a45aaca9561b95f1b6868c4c9255f82a1f18
Alec Collier <alec.collier@versent.com.au>
2019-07-05 14:25:06 +1000

    Refactoring.

--- kafkaclient.py
+++ kafkaclient.py
@@ -1,4 +1,4 @@
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaConsumer
 from kafka import KafkaAdminClient
 from kafka.admin import NewTopic
 
@@ -8,18 +8,35 @@ msk_endpoint_plaintext='b-2.alec-mykafka-poc.mktj7c.c4.kafka.ap-southeast-2.amaz
 
 msk_endpoint=msk_endpoint_plaintext
 
+TOPIC_NAME = 'ac_topic'
+producer = KafkaProducer(bootstrap_servers=msk_endpoint)
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=msk_endpoint)
+admin = KafkaAdminClient(bootstrap_servers=msk_endpoint)
+
 # Functions
 def send_data():
-    producer = KafkaProducer(bootstrap_servers=msk_endpoint)
-    for _ in range(100):
-        producer.send('ac_topic', b'some_message_bytes')
+  for _ in range(100):
+    producer.send(TOPIC_NAME, value=b'some_message_bytes')
 
+def consume_data():
+  for _ in range(100):
+    messages = consumer.poll(max_records=1)
+    return messages
 
 def create_topic(topic_name):
-    admin = KafkaAdminClient(bootstrap_servers=msk_endpoint)
-    topic_list = []
-    topic_list.append(NewTopic(name="ac_topic", num_partitions=1, replication_factor=1))
-    admin.create_topics(topic_list, None, False)
+  topic_list = []
+  topic_list.append(NewTopic(name=topic_name, num_partitions=1, replication_factor=1))
+  admin.create_topics(topic_list, None, False)
+
+def print_positions():
+  for topicPartition in consumer.assignment():
+    topic = topicPartition.topic
+    partition = topicPartition.partition
+    print('Topic {} Partition {}: position {}'.format(topic, partition, consumer.position(topicPartition)))
 
 # MAIN
-send_data()
+create_topic(TOPIC_NAME)
+#send_data()
+messages = consume_data()
+print(messages)
+
